e7fe6e663e1b922cd172aea59de4ecf72f31a62c
CR7Rubén <ra.gonzalez.c.cb188@gmail.com>
2026-03-15 19:58:56 -0700

    solución en el error del auth

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { register, login, updateProfile } from '../controllers/auth.controller.js';
-import { authenticateToken } from '../middleware/auth.middleware.js';
+import { authenticateToken } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
